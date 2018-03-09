09d3caf8f26cdacdfdb2e9130374783ddb49913b
bhaumik-choksi <bhaumik75@gmail.com>
2018-03-09 10:01:50 +0530

    Web3 Interface Updated

--- Contract/LendingInterface.py
+++ Contract/LendingInterface.py
@@ -3,6 +3,7 @@ from web3 import Web3
 from solc import compile_source
 from web3.contract import ConciseContract
 
+# Run-only-once-in-the-start part begins
 src = open('P2PLending.sol')
 src = "\n".join(list(line.replace("\n", "") for line in src))
 print("Contract Code Loaded")
@@ -23,7 +24,9 @@ contract_read_interface = w3provider.eth.contract(contract_interface['abi'], con
 contract_txn_interface = P2PLending_contract(contract_address)
 
 
-def createAccount(password, name, borrower=True):
+# Run-only-once-in-the-start part ends
+
+def create_account(password, name, borrower):
     account_address = w3provider.personal.newAccount(password)
 
     # Give some ether to fund gas for txns
@@ -33,34 +36,40 @@ def createAccount(password, name, borrower=True):
     # Unlock for 3 seconds to run the create<User> Txns
     w3provider.personal.unlockAccount(account_address, password, 3)
 
-    if borrower:
-        contract_txn_interface.call({'from': account_address, 'gas': 4100000}).createBorrower(name)
+    if borrower == "True":
+        contract_txn_interface.transact({'from': account_address, 'gas': 4100000}).createBorrower(name)
     else:
-        contract_txn_interface.call({'from': account_address, 'gas': 4100000}).createInvestor(name)
+        contract_txn_interface.transact({'from': account_address, 'gas': 4100000}).createInvestor(name)
 
     return account_address
 
 
-def login(address, password):
+def login(account_address, password):
+    # TODO: Store address in session variable
     try:
-        w3provider.personal.unlockAccount(address, password, 3600)
-        # TODO: Return customer type from contract for dynamic UI
-        # TODO: Store address in session variable
-        return [True]
+        w3provider.personal.unlockAccount(account_address, password, 3600)
+        is_borrower = contract_read_interface.isBorrower(account_address)
+        is_investor = contract_read_interface.isInvestor(account_address)
+        if is_borrower:
+            return [True, "borrower"]
+        elif is_investor:
+            return [True, "investor"]
+        else:
+            return [False, "INVALID"]
     except:
-        return [False]
+        return [False, None]
 
 
-def logout(address):
+def logout(account_address):
     try:
-        web3.personal.lockAccount(address)
+        web3.personal.lockAccount(account_address)
         # TODO: Destroy the session variable that stores the address
         return True
     except:
         return False
 
 
-def checkBalance(account_address):
+def check_balance(account_address):
     # TODO: On login, save account_address to global, maybe?
     return contract_txn_interface.call({'from': account_address, 'gas': 4100000}).viewBalance()
 
@@ -79,18 +88,47 @@ def deposit(account_address, amount):
 
 
 def squishify(*args, **kwargs):
-    # TODO: Change this to actually do something
-    return "1~2~3~4"
+    name = kwargs['name']
+    age = kwargs['age']
+    job = kwargs['job']
+    gender = kwargs['gender']
+    marr_status = kwargs['marr_status']
+    liab = kwargs['liab']
+    housing = kwargs['housing']
+    res_since = kwargs['res_since']
+    status_ca = kwargs['status_ca']
+    duration = kwargs['duration']
+    purpose = kwargs['purpose']
+    cred_amt = kwargs['cred_amt']
+    sav_act = kwargs['sav_act']
+    emp_since = kwargs['emp_since']
+    inst_rate = kwargs['inst_rate']
+    debtors = kwargs['debtors']
+    _property = kwargs['_property']
+    plans = kwargs['plans']
+    exist_cred = kwargs['exist_cred']
+    phone = kwargs['phone']
+    foreign = kwargs['foreign']
+    return "~".join([name, age, job, gender, marr_status, liab, housing, res_since, status_ca, duration, purpose,
+                     cred_amt, sav_act, emp_since, inst_rate, debtors, _property, plans, exist_cred, phone, foreign])
 
 
 def desquishify(compressed_string):
-    # TODO: Change this to actually do something
-    return compressed_string.split("~")
+    s = compressed_string.split('~')
+    return {
+        'name': s[0], 'age': s[1], 'job': s[2],
+        'gender': s[3], 'marr_status': s[4], 'liab': s[5],
+        'housing': s[6], 'res_since': s[7], 'status_ca': s[8],
+        'duration': s[9], 'purpose': s[10], 'cred_amt': s[11],
+        'sav_act': s[12], 'emp_since': s[13], 'inst_rate': [14],
+        'debtors': s[15], '_property': s[16], 'plans': s[17],
+        'exist_cred': s[18], 'phone': s[19], 'foreign': s[20]
+    }
 
 
-def createApplication(account_address, duration, interest_rate, credit_amount, otherData):
+def create_application(account_address, duration, interest_rate, credit_amount, otherData):
     try:
-        contract_txn_interface.transact({'from': account_address, 'gas': 4100000}) \
+        contract_txn_interface.transact({'from': account_address}) \
             .createApplication(int(duration), int(interest_rate), int(credit_amount), otherData)
         return True
     except:
@@ -105,8 +143,226 @@ def get_num_loans():
     return contract_txn_interface.call({'from': w3provider.eth.accounts[0], 'gas': 4100000}).getNumLoans()
 
 
-add = createAccount(input("Enter Password"), input("Enter name"), borrower=True)
-print(login(add, input("Enter Password to Login")))
-print("Creating application", createApplication(add, 10, 1, 100, squishify()))
-print("Num Loans", get_num_loans())
-print("Num Applications", get_num_applications())
+def view_open_applications():
+    open_applications = []
+    n = get_num_applications()
+    for i in range(1, n + 1):
+        is_open = contract_read_interface.ifApplicationOpen(i)
+        if is_open:
+            raw_data = contract_read_interface.getApplicationData(i)
+            open_applications.append({
+                'application_id': raw_data[0][0],
+                'duration': raw_data[0][1],
+                'amount': raw_data[0][2],
+                'interest_rate': raw_data[0][3],
+                # TODO: De-squishify other data
+                'other_data': raw_data[1],
+                'borrower': raw_data[2]
+            })
+    return open_applications
+
+
+def view_open_applications():
+    open_applications = []
+    n = get_num_applications()
+    for i in range(1, n + 1):
+        is_open = contract_read_interface.ifApplicationOpen(i)
+        if is_open:
+            raw_data = contract_read_interface.getApplicationData(i)
+            other_data = desquishify(raw_data[1])
+            open_applications.append({
+                'application_id': raw_data[0][0],
+                'duration': raw_data[0][1],
+                'amount': raw_data[0][2],
+                'interest_rate': raw_data[0][3],
+                'name': other_data['name'],
+                'purpose': other_data['purpose'],
+                'job': other_data['job'],
+                'age': other_data['age'],
+                'category': 2,
+                'confidence': 95,
+                'borrower': raw_data[2]
+            })
+    return open_applications
+
+
+def grant_loan(account_address, app_id):
+    try:
+        contract_txn_interface.transact({'from': account_address}).grantLoan(int(app_id))
+        return True
+    except:
+        return False
+
+
+def view_my_loan(account_address, userType):
+    if userType == "borrower":
+        n = get_num_loans()
+        for i in range(1, n + 1):
+            is_open = contract_read_interface.ifLoanOpen(i)
+            if is_open:
+                raw_data = contract_read_interface.getLoanData(i)
+                if raw_data[1].lower() == account_address.lower():
+                    return {
+                        'loan_id': raw_data[0][0],
+                        'interest_rate': raw_data[0][1],
+                        'duration': raw_data[0][2],
+                        'principal_amount': raw_data[0][3],
+                        'original_amount': raw_data[0][4],
+                        'paid_amount': raw_data[0][5],
+                        'start_time': raw_data[0][6],
+                        'monthly_checkpoint': raw_data[0][7],
+                        'application_id': raw_data[0][8],
+                        'borrower': raw_data[1],
+                        'investor': raw_data[2]
+                    }
+        return False
+    elif userType == "investor":
+        n = get_num_loans()
+        for i in range(1, n + 1):
+            is_open = contract_read_interface.ifLoanOpen(i)
+            if is_open:
+                raw_data = contract_read_interface.getLoanData(i)
+                if raw_data[2].lower() == account_address.lower():
+                    return {
+                        'loan_id': raw_data[0][0],
+                        'interest_rate': raw_data[0][1],
+                        'duration': raw_data[0][2],
+                        'principal_amount': raw_data[0][3],
+                        'original_amount': raw_data[0][4],
+                        'paid_amount': raw_data[0][5],
+                        'start_time': raw_data[0][6],
+                        'monthly_checkpoint': raw_data[0][7],
+                        'application_id': raw_data[0][8],
+                        'borrower': raw_data[1],
+                        'investor': raw_data[2]
+                    }
+        return False
+    else:
+        return False
+
+
+def estimate_interest(account_address, userType):
+    # Select how fast you want time to move
+    TIME_STEP = {'seconds': 1, 'minutes': 60, 'hours': 3600, 'days': 3600 * 24, 'months': 3600 * 24 * 30}
+
+    if userType == "investor":
+        return False
+    else:
+        loan_data = view_my_loan(account_address, userType)
+        if loan_data != False:
+            p = loan_data['principal_amount']
+            r = loan_data['interest_rate']
+            checkpoint = loan_data['monthly_checkpoint']
+            start_time = loan_data['start_time']
+            n = 12  # n is the number of times interest is compounded annually
+            amount_with_interest = p
+
+            # Note that this time is not the actual time, but a timestamp in num_of_seconds
+            current_time = contract_read_interface.getTime()
+            time_elapsed = (current_time - start_time) / TIME_STEP['seconds']
+            t_since_last_payment = time_elapsed - checkpoint
+
+            print(current_time, "Current Time")
+            print(start_time, "Start time")
+            print(checkpoint, "Checkpoint")
+            print(time_elapsed, "Time elapsed")
+
+            if t_since_last_payment != 0:
+                amount_with_interest = p * (1 + r / (100 * n)) ** (n * time_elapsed / 12)
+                amount_with_interest = int(amount_with_interest)
+                return [int(amount_with_interest), int(t_since_last_payment)]
+            else:
+                return [0, 0]
+
+
+def repay_loan(account_address, amount):
+    amount_with_interest, months_since_last_checkpoint = estimate_interest(account_address, 'borrower')
+    amount = int(amount)  # Typecast for safety
+    contract_txn_interface.transact({'from': account_address}) \
+        .repayLoan(amount, amount_with_interest, months_since_last_checkpoint)
+    return True
+
+
+def view_application_by_id(index):
+    n = get_num_applications()
+    if index > n or index < 1:
+        return False
+    else:
+        raw_data = contract_read_interface.getApplicationData(index)
+        return desquishify(raw_data[1])
+
+
+def view_loan_by_id(index):
+    n = get_num_loans()
+    if index > n or index < 1:
+        return False
+    else:
+        raw_data = contract_read_interface.getLoanData(index)
+        return {
+            'loan_id': raw_data[0][0],
+            'interest_rate': raw_data[0][1],
+            'duration': raw_data[0][2],
+            'principal_amount': raw_data[0][3],
+            'original_amount': raw_data[0][4],
+            'paid_amount': raw_data[0][5],
+            'start_time': raw_data[0][6],
+            'monthly_checkpoint': raw_data[0][7],
+            'application_id': raw_data[0][8],
+            'borrower': raw_data[1],
+            'investor': raw_data[2]
+        }
+
+
+def get_full_blockchain():
+    n = w3provider.eth.blockNumber
+    blocks_with_txns = []
+    for i in range(10):
+        block = w3provider.eth.getBlock(i)
+        blocks_with_txns.append(block)
+
+    return blocks_with_txns
+
+
+def view_my_txns_from_blockchain(account_address):
+    n = w3provider.eth.blockNumber
+    print(n, "Blocks in the blockchain")
+    output = {}
+    for block_num in range(n):
+        output[block_num] = []
+        txn_count = w3provider.eth.getBlockTransactionCount(block_num)
+        for i in range(txn_count):
+                output[block_num].append(w3provider.eth.getTransactionFromBlock(block_num, i))
+
+    return output
+
+
+# Tester code below
+bor = create_account("foo", "foo", "True")
+login(bor, "foo")
+print("Creating application", create_application(bor, 24, 5, 100, 'x'))
+# print(deposit(bor, 5000), "Deposit")
+#
+# inv = create_account("bar", "bar", "False")
+# login(inv, "bar")
+# deposit(inv, 100000)
+# print("Granting Loan", grant_loan(inv, 1))
+
+# Create Dummy Applications
+# for i in range(5):
+#     dummy = create_account("foo", "foo", "True")
+#     login(dummy, "foo")
+#     create_application(dummy, 24, 5, 100, 'x')
+#     print("Dummy Application Created")
+
+#
+# amount_with_interest, months_since_last_checkpoint = estimate_interest(bor, 'borrower')
+# print(repay_loan(bor, amount_with_interest))
+# print(view_my_loan(inv, "investor"))
+
+# op = view_my_txns_from_blockchain(bor)
+# for k,v  in op.items():
+#     print(k,v)
+
+keyss = list(k for k in w3provider.eth.getBlock(2))
+print("\n".join(keyss))
+print(keyss)
--- Contract/P2PLending.sol
+++ Contract/P2PLending.sol
@@ -26,13 +26,11 @@ contract P2PLending {
         string name;
         bool EXISTS;
     }
-
     struct Borrower{
         address borrower_public_key;
         string name;
         bool EXISTS;
     }
-
     struct LoanApplication{
         //For traversal and indexing
         bool openApp;
@@ -45,7 +43,6 @@ contract P2PLending {
         string otherData; // Encoded string with delimiters (~)
 
     }
-
     struct Loan{
 
         //For traversal and indexing
@@ -60,17 +57,16 @@ contract P2PLending {
         uint original_amount;
         uint amount_paid;
         uint startTime;
+        uint monthlyCheckpoint;
         uint appId;
 
     }
-
     // Methods
     function P2PLending(){
         // TODO Constructor may be added later
         numLoans = 1;
         numApplications = 1;
     }
-
     function createInvestor(string name){
         Investor investor;
         investor.name = name;
@@ -83,7 +79,6 @@ contract P2PLending {
         balances[msg.sender] = 0; // Init balance
 
     }
-
     function createBorrower(string name){
         Borrower borrower;
         borrower.name = name;
@@ -96,31 +91,27 @@ contract P2PLending {
         hasOngoingApplication[msg.sender] = false;
         balances[msg.sender] = 0; // Init balance
     }
-
     function viewBalance() public returns (uint){
         return balances[msg.sender];
     }
-
     function deposit(uint amount) {
         balances[msg.sender] += amount;
     }
-
     function withdraw(uint amount) returns (uint) {
         require(amount <= balances[msg.sender]);
         balances[msg.sender] -= amount;
         return amount;
     }
-
     function transfer(address giver, address taker, uint amount){
         require(balances[giver] >= amount);
         balances[giver] -= amount;
         balances[taker] += amount;
     }
-
     function createApplication(uint duration, uint interest_rate, uint credit_amount, string otherData){
 
         require(hasOngoingLoan[msg.sender] == false);
         require(hasOngoingApplication[msg.sender] == false);
+        require(isBorrower(msg.sender));
         applications[numApplications] = LoanApplication(true, numApplications, msg.sender, duration, credit_amount, interest_rate, otherData);
         // app.duration = duration;
         // app.interest_rate = interest_rate;
@@ -134,7 +125,6 @@ contract P2PLending {
         numApplications += 1;
         hasOngoingApplication[msg.sender] = true;
     }
-
     function grantLoan(uint appId){
         //Check sufficient balance
         require(balances[msg.sender] >= applications[appId].credit_amount);
@@ -145,7 +135,8 @@ contract P2PLending {
         balances[applications[appId].borrower] += applications[appId].credit_amount;
 
         // Populate loan object
-        loans[numLoans] = Loan(true, numLoans, applications[appId].borrower, msg.sender, applications[appId].interest_rate, applications[appId].duration, applications[appId].credit_amount, applications[appId].credit_amount, 0, now, appId);
+        loans[numLoans] = Loan(true, numLoans, applications[appId].borrower, msg.sender, applications[appId].interest_rate, applications[appId].duration,
+        applications[appId].credit_amount, applications[appId].credit_amount, 0, now,0, appId);
         numLoans += 1;
 
         applications[appId].openApp = false;
@@ -155,9 +146,7 @@ contract P2PLending {
 
     }
 
-    function repayLoan(uint amount){
-
-
+    function repayLoan(uint amount, uint estimatedInterest, uint timeSinceLastPayment){
         require(balances[msg.sender] >= amount);
         uint id_ = 0;
         for(uint i=1; i<=numLoans; i++)
@@ -173,17 +162,28 @@ contract P2PLending {
 
         uint p = loan.principal_amount;
         uint r = loan.interest_rate;
-        uint t = loan.duration;
+        uint checkpoint = loan.monthlyCheckpoint;
         uint n = 12;
 
-        //uint finalAmount = p * (1 + r/n)**(n*t);
-        //uint interest = p * (1 + r/n)**(n*)   1/12 2/12 3/12 --- duration/12
+        uint amountWithInterest;
+        uint interest;
 
-        //bhaumik
-        uint timeElapsedInMonths = (now - loan.startTime);//(3600*24*30);
-        timeElapsedInMonths = 1;
-        // //3600 seconds in an hour, 24 hours a day, 30 days a month
-        uint interest = p*(r)*(timeElapsedInMonths);
+
+        // uint timeElapsedInMonths = (now - loan.startTime)/(3600*24*30);
+        // uint t = timeElapsedInMonths - checkpoint;
+        uint t = timeSinceLastPayment;
+        if (t!=0)
+        {
+            amountWithInterest = estimatedInterest;
+            interest = amountWithInterest - p;
+            loan.monthlyCheckpoint += t;
+        }
+        else
+        {
+            amountWithInterest = p;
+        }
+
+
         // Check if amount is greater than monthly installment
         require(interest <=  amount);
 
@@ -193,9 +193,10 @@ contract P2PLending {
 
         // Add amount to total amount paid
         loan.amount_paid += amount;
-        amount -= interest;
+
 
         require(loan.principal_amount >= amount);
+        loan.principal_amount = amountWithInterest - amount;
         loan.principal_amount -= amount;
 
         if(loan.principal_amount == 0)
@@ -207,17 +208,14 @@ contract P2PLending {
         }
         //bhaumik
     }
-
     function ifApplicationOpen(uint index) returns (bool){
-        if(applications[index].openApp==true) return true;
-        else return false;
+        LoanApplication app = applications[index];
+        if(app.openApp) return true; else return false;
     }
-
     function ifLoanOpen(uint index) returns (bool){
-        if(loans[index].openLoan==true) return true;
-        else return false;
+        Loan loan = loans[index];
+        if (loan.openLoan == true) return true; else return false;
     }
-
     function getApplicationData(uint index) returns (uint[], string, address){
         string otherData = applications[index].otherData;
         uint[] memory numericalData = new uint[](4);
@@ -230,9 +228,8 @@ contract P2PLending {
         return (numericalData, otherData, borrower);
         // numericalData format = [index, duration, amount, interestrate]
     }
-
     function getLoanData(uint index) returns (uint[], address, address){
-        uint[] memory numericalData = new uint[](8);
+        uint[] memory numericalData = new uint[](9);
         numericalData[0] = index;
         numericalData[1] = loans[index].interest_rate;
         numericalData[2] = loans[index].duration;
@@ -240,12 +237,42 @@ contract P2PLending {
         numericalData[4] = loans[index].original_amount;
         numericalData[5] = loans[index].amount_paid;
         numericalData[6] = loans[index].startTime;
-        numericalData[7] = loans[index].appId;
+        numericalData[7] = loans[index].monthlyCheckpoint;
+        numericalData[8] = loans[index].appId;
 
         return (numericalData, loans[index].borrower, loans[index].investor);
         // numericalData format = [index, interestrate, duration, p_amnt, o_amnt, paid_amnt, starttime, app_index]
     }
+    function estimateInterest(uint index) returns (uint){
+        uint id_ = 0;
+        for(uint i=1; i<=numLoans; i++)
+        {
+                if(loans[i].borrower == msg.sender)
+                {
+                    id_ = i;
+                    break;
+                }
+        }
+        Loan loan = loans[id_];
+        require(loan.openLoan == true);
 
+        uint p = loan.principal_amount;
+        uint r = loan.interest_rate;
+        uint checkpoint = loan.monthlyCheckpoint;
+        uint n = 12;
+        uint amountWithInterest = p;
+
+        uint timeElapsedInMonths = (now - loan.startTime)/(3600*24*30);
+        uint t = timeElapsedInMonths - checkpoint;
+        if (t!=0)
+        {
+            amountWithInterest = p * (1 + r/(100*n))**(n*t/12);
+        }
+        return (amountWithInterest);
+    }
     function getNumApplications() returns (uint) { return numApplications;}
     function getNumLoans() returns (uint) { return numLoans;}
+    function isInvestor(address account) returns (bool) {return investors[account].EXISTS;}
+    function isBorrower(address account) returns (bool) {return borrowers[account].EXISTS;}
+    function getTime() returns (uint){return now;}
 }
